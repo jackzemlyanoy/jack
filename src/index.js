497a1ad75cf7e01910637f94f1ea031cf6c0be88
jackzemlyanoy <39563987+jackzemlyanoy@users.noreply.github.com>
2018-07-19 14:38:10 +0300

    add state to article

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import Article from './Article';
+import {articles} from "./fixtures";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Article article = {articles[0]} />, document.getElementById('root'));
