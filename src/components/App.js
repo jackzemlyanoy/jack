81c75a013413163c7437453aa3f5b1bd09fe09d0
jackzemlyanoy <39563987+jackzemlyanoy@users.noreply.github.com>
2018-07-30 14:50:07 +0300

    add calendar Data and select

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import ArticleList from './ArticleList';
 import UserForm from './UserForm';
 import Select from 'react-select';
+import Data from "./Data";
+
 
 class App extends Component {
+    state = {
+        selection: null
+    }
+
   render() {
+      const options = this.props.articles.map(article => ({
+          label: article.title,
+          value: article.id
+      }))
     return (
       <div>
         <UserForm />
-        <Select />
+        <Select options = {options} value = {this.state.selection} onChange = {this.changeSelection} isMulti={true}/>
+        <Data />
         <ArticleList articles={this.props.articles} />
       </div>
     );
   }
+  changeSelection = selection => this.setState({selection})
 }
 
 export default App;
